2756c63c541109266095f287cd7d081039a4a2c0
Ander Ustarroz <anderustarroz@gmail.com>
2023-09-29 08:43:48 +0930

    Added multi-app support

--- programs/sol-cerberus-demo/src/lib.rs
+++ programs/sol-cerberus-demo/src/lib.rs
@@ -13,9 +13,6 @@ const PROGRAM_AUTHORITY: Pubkey = pubkey!("SCfVPLT34pep4pHfnMTzSyMZ2kLcxjKTGS2ph
 
 declare_id!("testX83crd4vAgRrvmwXgVQ2r69uCpg8xzh8A5X124x");
 
-// WARNING: Never use the following Pubkey on production!!!
-const SOL_CERBERUS_APP_ID: Pubkey = pubkey!("testX83crd4vAgRrvmwXgVQ2r69uCpg8xzh8A5X124x");
-
 #[program]
 pub mod sol_cerberus_demo {
 
@@ -31,46 +28,55 @@ pub mod sol_cerberus_demo {
 
     #[rule(Square, Add)]
     pub fn add_square(ctx: Context<Add>, color: String, size: u16) -> Result<()> {
+        let sol_cerberus_app_id: Pubkey = ctx.accounts.demo.sol_cerberus_app;
         instructions::add::add(ctx, "square", &color, size)
     }
 
     #[rule(Circle, Add)]
     pub fn add_circle(ctx: Context<Add>, color: String, size: u16) -> Result<()> {
+        let sol_cerberus_app_id: Pubkey = ctx.accounts.demo.sol_cerberus_app;
         instructions::add::add(ctx, "circle", &color, size)
     }
 
     #[rule(Triangle, Add)]
     pub fn add_triangle(ctx: Context<Add>, color: String, size: u16) -> Result<()> {
+        let sol_cerberus_app_id: Pubkey = ctx.accounts.demo.sol_cerberus_app;
         instructions::add::add(ctx, "triangle", &color, size)
     }
 
     #[rule(Square, Update)]
     pub fn update_square(ctx: Context<Update>, color: String, size: u16) -> Result<()> {
+        let sol_cerberus_app_id: Pubkey = ctx.accounts.demo.sol_cerberus_app;
         instructions::update::update(ctx, "square", &color, size)
     }
 
     #[rule(Circle, Update)]
     pub fn update_circle(ctx: Context<Update>, color: String, size: u16) -> Result<()> {
+        let sol_cerberus_app_id: Pubkey = ctx.accounts.demo.sol_cerberus_app;
         instructions::update::update(ctx, "circle", &color, size)
     }
 
     #[rule(Triangle, Update)]
     pub fn update_triangle(ctx: Context<Update>, color: String, size: u16) -> Result<()> {
+        let sol_cerberus_app_id: Pubkey = ctx.accounts.demo.sol_cerberus_app;
         instructions::update::update(ctx, "triangle", &color, size)
     }
 
     #[rule(Square, Delete)]
     pub fn delete_square(ctx: Context<Delete>) -> Result<()> {
+        let sol_cerberus_app_id: Pubkey = ctx.accounts.demo.sol_cerberus_app;
         instructions::delete::delete(ctx, "square")
     }
 
     #[rule(Circle, Delete)]
     pub fn delete_circle(ctx: Context<Delete>) -> Result<()> {
+        let sol_cerberus_app_id: Pubkey = ctx.accounts.demo.sol_cerberus_app;
         instructions::delete::delete(ctx, "circle")
     }
 
     #[rule(Triangle, Delete)]
     pub fn delete_triangle(ctx: Context<Delete>) -> Result<()> {
+        let sol_cerberus_app_id: Pubkey = ctx.accounts.demo.sol_cerberus_app;
         instructions::delete::delete(ctx, "triangle")
     }
 }
